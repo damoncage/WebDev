cc35516549c93b8037a93318a047c72f4f93b64c
cage <damoncageorz@gmail.com>
2016-02-18 23:50:11 -0500

    Added new homepage into public/experiment/portfolio, tried the AngularJS

---
+++ public/experiment/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cage on 2/18/16.
+ */
+var app = angular.module("HelloWorldApp", []);
+
+app.controller("HelloWorldController", HelloWorldController);
+
+function HelloWorldController($scope) {
+    $scope.hello = "Hello World from AngularJS";
+}
